14f68da8d2c450d11c0370e9e2d4bcd912648e53
tehrelt <haxrelt@gmail.com>
2022-01-14 14:50:38 +0300

    Тестирование ViewModel

--- XYZ/ViewModels/MainWindowViewModel.cs
+++ XYZ/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,16 @@
-﻿namespace XYZ.ViewModels
-{
-    internal class MainWindowViewModel
-    {
+﻿using XYZ.ViewModels.Base;
 
+namespace XYZ.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _title = "Анализ статистики XYZ";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
     }
 }
