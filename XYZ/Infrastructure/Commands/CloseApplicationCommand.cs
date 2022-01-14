4ff8cb89cdce200c074e3a6dc2a03c59ff712bd9
tehrelt <haxrelt@gmail.com>
2022-01-14 16:28:06 +0300

    Определение класса отдельной команды

---
+++ XYZ/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+using XYZ.Infrastructure.Commands.Base;
+
+namespace XYZ.Infrastructure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+        public override void Execute(object parameter) => Application.Current.Shutdown();
+    }
+}
