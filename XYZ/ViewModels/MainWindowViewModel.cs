a0b61fab5b1b5dcab8fa0418ce8298034e78ea94
tehrelt <haxrelt@gmail.com>
2022-01-14 15:54:27 +0300

    Привязка Статуса к разметке

--- XYZ/ViewModels/MainWindowViewModel.cs
+++ XYZ/ViewModels/MainWindowViewModel.cs
@@ -4,6 +4,7 @@ namespace XYZ.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region Title : string - Заголовок окна
         private string _title = "Анализ статистики XYZ";
         /// <summary>Заголовок окна</summary>
         public string Title
@@ -11,5 +12,16 @@ namespace XYZ.ViewModels
             get => _title;
             set => Set(ref _title, value);
         }
+        #endregion
+
+        #region Status : string - Статус программы
+        private string _status = "Готов!";
+        /// <summary>Статус Програмы</summary>
+        public string Status
+        {
+            get => _status;
+            set => Set(ref _status, value);
+        }
+        #endregion
     }
 }
