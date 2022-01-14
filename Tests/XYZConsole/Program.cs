d114eddd3f48541864b0d83192fd66809ca34e6e
tehrelt <haxrelt@gmail.com>
2022-01-14 18:04:24 +0300

    Извлечение дат

--- Tests/XYZConsole/Program.cs
+++ Tests/XYZConsole/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
-using System.Net;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace XYZConsole
 {
@@ -10,11 +14,48 @@ namespace XYZConsole
 
         static void Main(string[] args)
         {
-            var client = new HttpClient();
-            var response = client.GetAsync(DATA_URL).Result;
-            var contentCSV = response.Content.ReadAsStringAsync().Result;
+            //var client = new HttpClient();
+            //var response = client.GetAsync(DATA_URL).Result;
+            //var contentCSV = response.Content.ReadAsStringAsync().Result;
+
+            //foreach (var dataLine in GetDataLines())
+            //{
+            //    Console.WriteLine(dataLine);
+            //}
+
+            var dates = GetDates();
+
+            Console.WriteLine(string.Join("\r\n", dates));
 
             Console.ReadLine();
         }
+        private static async Task<Stream> GetDataStream()
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(DATA_URL, HttpCompletionOption.ResponseHeadersRead);
+            return await response.Content.ReadAsStreamAsync();
+        }
+
+        private static IEnumerable<string> GetDataLines()
+        {
+            using var dataStream = GetDataStream().Result;
+            using var dataReader = new StreamReader(dataStream);
+
+            while (!dataReader.EndOfStream)
+            {
+                var line = dataReader.ReadLine();
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                yield return line;
+            }
+        }
+
+        private static DateTime[] GetDates() => GetDataLines()
+            .First()
+            .Split(',')
+            .Skip(4)
+            .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
+            .ToArray();
+
+
     }
 }
