0d67bca45047652954994eafa86e85a4948a9a3b
tehrelt <haxrelt@gmail.com>
2022-01-14 17:46:25 +0300

    Запрос к удалённому серверу

--- Tests/XYZConsole/Program.cs
+++ Tests/XYZConsole/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
 
 namespace XYZConsole
 {
     internal class Program
     {
+        private const string DATA_URL = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new HttpClient();
+            var response = client.GetAsync(DATA_URL).Result;
+            var contentCSV = response.Content.ReadAsStringAsync().Result;
+
+            Console.ReadLine();
         }
     }
 }
